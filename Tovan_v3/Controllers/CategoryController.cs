e7471a4de5c3466dde18e245ee45bd445dbd993c
Martin <Martin@CARBONARA-DATOR>
2018-02-26 13:43:27 +0100

    Add project files.

---
+++ Tovan_v3/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tovan_v3.Models;
+
+namespace Tovan_v3.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private project_tovanEntities db = new project_tovanEntities();
+
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Tovan_v3/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Tovan_v3.Models;
+using System.IO;
+
+
+namespace Tovan_v3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private project_tovanEntities db = new project_tovanEntities();
+        // GET: Home
+        [HttpGet]
+        public ActionResult Index()
+        {
+            ViewBag.imageCategory = new SelectList(db.Tbl_category, "categoryID", "categoryType");
+            return View();
+        }
+
+        [HttpPost]
+        /*public ActionResult Index(HttpPostedFileBase imageSrc, FormCollection imageCategory)
+        {
+
+           
+           if (imageSrc != null)
+            {
+                string path = Server.MapPath("~/Uploads/");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                imageSrc.SaveAs(path + Path.GetFileName(imageSrc.FileName));
+                ViewBag.Message = imageCategory["category"].ToString();
+                ViewBag.Image = "/Uploads/" + Path.GetFileName(imageSrc.FileName);
+
+                
+                if (ModelState.IsValid)
+                {
+                    db.Tbl_images.Add(tbl_images);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+
+
+            }
+
+            return View();
+        }*/
+        public ActionResult Index([Bind(Include = "imageID, imageSrc, imageCategory")] Tbl_images tbl_images)
+        { 
+                if (ModelState.IsValid)
+                {
+                    db.Tbl_images.Add(tbl_images);
+                    db.SaveChanges();
+                }
+
+            return View();
+        }
+    }
+}
